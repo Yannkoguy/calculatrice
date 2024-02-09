b0f1b866ceabb6a9af696402ad3617a796ff7946
Yannkoguy <yann.koguy@gmail.com>
2024-02-09 00:45:10 +0000

    création des fonctions basiques de javascript pour les opérations +, -, *,/ puis developpement de l'ui disigne de la calculatrice avec du html et du css

---
+++ main.js
@@ -0,0 +1,27 @@
+let firstN;
+let secodN;
+let operator;
+
+const addition = function (a, b) {
+  return a + b;
+};
+const Multipy = function (a, b) {
+  return a * b;
+};
+const subtract = function (a, b) {
+  return a - b;
+};
+const divide = function (a, b) {
+  return a / b;
+};
+
+let operate;
+
+operate = function (firstN, secodN, operator) {
+  return firstN, operator, secodN;
+};
+
+console.log(addition(5, 6));
+console.log(Multipy(10, 6));
+console.log(subtract(10, 6));
+console.log(divide(10, 2));
