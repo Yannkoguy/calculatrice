4dc7f785cbc01d45ec70de7e7f7243dea5b49f49
Yannkoguy <yann.koguy@gmail.com>
2024-02-10 16:26:59 +0000

    developpement continue de la calculatrice , ajout de nouvelles fonction tels que operate et  création des conteneurs variables

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
 let firstN;
 let secodN;
 let operator;
+let resultat;
 
-const addition = function (a, b) {
+const screen = document.getElementById("screen");
+const numbers = document.getElementsByClassName("number");
+const operators = document.getElementsByClassName("operator");
+
+function addition(a, b) {
   return a + b;
-};
+}
 const Multipy = function (a, b) {
-  return a * b;
+  return (resultat = a * b);
 };
 const subtract = function (a, b) {
-  return a - b;
+  return (resultat = a - b);
 };
 const divide = function (a, b) {
-  return a / b;
+  return (resultat = a / b);
 };
 
-let operate;
-
-operate = function (firstN, secodN, operator) {
-  return firstN, operator, secodN;
-};
+function operate() {
+  if (operator == "+") {
+    addition(firstN, secodN);
+  } else if (operator == "-") {
+    subtract(firstN, secodN);
+  } else if (operator == "*") {
+    Multipy(firstN, secodN);
+  } else {
+    divide(firstN, secodN);
+  }
+}
 
 console.log(addition(5, 6));
 console.log(Multipy(10, 6));
